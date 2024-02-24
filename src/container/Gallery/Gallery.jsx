a08a5763b5039ffcc578827527ab2ab5d6b0682b
Anand-1442 <anandsingh14442@gmail.com>
2024-02-25 01:05:25 +0530

    Laurels

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import { BsInstagram , BsArrowLeftShort, BsArrowRightShort} from 'react-icons/bs';
+
 
 import './Gallery.css';
 
